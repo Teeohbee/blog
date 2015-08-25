7ce782af19cf3e01242508f2062f9b90028ff06a
Richard Geoghegan <richard.geoghegan@ntlworld.com>
2015-08-25 14:47:15 +0100

    linkd relationships

--- spec/features/adding_tags_spec.rb
+++ spec/features/adding_tags_spec.rb
@@ -17,25 +17,25 @@ feature 'Adding tags' do
   end
 
   scenario 'I can add multiple tags to a new blog' do
-     user = User.first
-     visit '/blogs/new'
-     fill_in 'title', with: 'Title test'
-     fill_in 'content', with: 'Title content'
-     fill_in 'tags', with: 'space guns'
-     click_button 'Create Blog'
-     blog = user.blogs.first
-     expect(user.blogs.tags.map(&:name)).to include('space', 'guns')
- end
+    user = User.first
+    visit '/blogs/new'
+    fill_in 'title', with: 'Title test'
+    fill_in 'content', with: 'Title content'
+    fill_in 'tags', with: 'space guns'
+    click_button 'Create Blog'
+    blog = user.blogs.first
+    expect(user.blogs.tags.map(&:name)).to include('space', 'guns')
+  end
 
- def sign_up(user)
-   visit '/users/new'
-   fill_in :email, with: user.email
-   fill_in :username, with: user.username
-   fill_in :name, with: user.name
-   fill_in :phone, with: user.phone
-   fill_in :password, with: user.password
-   fill_in :password_confirmation, with: user.password_confirmation
-   click_button 'Sign up'
- end
+  def sign_up(user)
+    visit '/users/new'
+    fill_in :email, with: user.email
+    fill_in :username, with: user.username
+    fill_in :name, with: user.name
+    fill_in :phone, with: user.phone
+    fill_in :password, with: user.password
+    fill_in :password_confirmation, with: user.password_confirmation
+    click_button 'Sign up'
+  end
 
 end
--- spec/features/viewing_blogs_spec.rb
+++ spec/features/viewing_blogs_spec.rb
@@ -2,30 +2,30 @@ require 'spec_helper'
 require './data_mapper_setup'
 
 feature 'Viewing Bloggs' do
-  scenario 'I can see existing bloggs on the blogs page' do
-    Blog.create(content: 'First Blog', title: 'My First Blog')
-
-    visit '/blogs'
-
-    expect(page.status_code).to eq 200
-    within 'ul#blogs' do
-      expect(page).to have_content('My First Blog')
-    end
-  end
 
   before(:each) do
-    Blog.create(content: 'This is a new blog',
-                title: 'First Blog',
-                tags: [Tag.first_or_create(name: 'Sport')])
-    Blog.create(content: 'The tech industry',
-                title: 'Tech News',
-                tags: [Tag.first_or_create(name: 'Tech')])
-    Blog.create(content: 'The best holiday',
-                title: 'France is awesome',
-                tags: [Tag.first_or_create(name: 'Holidays')])
-    Blog.create(content: 'Never go abroad',
-                title: 'England is wet',
-                tags: [Tag.first_or_create(name: 'Holidays')])
+    sign_up(build :user)
+    user = User.first
+    user.blogs.create(content: 'This is a new blog',
+                      title: 'First Blog',
+                      tags: [Tag.first_or_create(name: 'Sport')])
+    user.blogs.create(content: 'The tech industry',
+                      title: 'Tech News',
+                      tags: [Tag.first_or_create(name: 'Tech')])
+    user.blogs.create(content: 'The best holiday',
+                      title: 'France is awesome',
+                      tags: [Tag.first_or_create(name: 'Holidays')])
+    user.blogs.create(content: 'Never go abroad',
+                      title: 'England is wet',
+                      tags: [Tag.first_or_create(name: 'Holidays')])
+  end
+
+  scenario 'I can see existing bloggs on the blogs page' do
+    visit '/blogs'
+    expect(page.status_code).to eq 200
+    within 'ul#blogs' do
+      expect(page).to have_content('This is a new blog')
+    end
   end
 
   scenario 'I can filter blogs by tags' do
@@ -36,5 +36,16 @@ feature 'Viewing Bloggs' do
       expect(page).to have_content('Holidays')
       expect(page).to have_content('Holidays')
     end
+  end
+
+  def sign_up(user)
+    visit '/users/new'
+    fill_in :email, with: user.email
+    fill_in :username, with: user.username
+    fill_in :name, with: user.name
+    fill_in :phone, with: user.phone
+    fill_in :password, with: user.password
+    fill_in :password_confirmation, with: user.password_confirmation
+    click_button 'Sign up'
   end
 end
